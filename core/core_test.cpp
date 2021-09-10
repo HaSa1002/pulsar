74626452fa9bb6bd2049aad5edb9a0eba93618a0
Johannes Witt <johawitt@outlook.de>
2021-09-10 15:25:39 +0200

    Check if file gets executed

--- core/core_test.cpp
+++ core/core_test.cpp
@@ -3,6 +3,7 @@
 #include "server/event.hpp"
 
 int main(int argc, char** argv) {
+	printf("Just a test");
 	doctest::Context context;
 	context.applyCommandLine(argc, argv);
 	context.setOption("no-breaks", true); 
