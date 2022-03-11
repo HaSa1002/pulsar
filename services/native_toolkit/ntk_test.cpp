a89580b91912d48428c677f3197fdc1efcb70320
Johannes Witt <johawitt@outlook.de>
2022-03-11 01:21:18 +0100

    Reenable tests and remove main boilerplate

--- services/native_toolkit/ntk_test.cpp
+++ services/native_toolkit/ntk_test.cpp
@@ -1,11 +1,2 @@
-#define DOCTEST_CONFIG_IMPLEMENT
-#define DOCTEST_CONFIG_TREAT_CHAR_STAR_AS_STRING
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "window.hpp"
-
-int main(int argc, char** argv) {
-	doctest::Context context;
-	context.applyCommandLine(argc, argv);
-	context.setOption("no-breaks", true); 
-	return context.run();
-}
